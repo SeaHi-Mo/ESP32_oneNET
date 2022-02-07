173d113a9294eba2f252186e2c97742942d56dc4
SeaHi-Mo <seahi-mo@foxmail.com>
2022-02-07 17:29:07 +0800

    frist commit

---
+++ main/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "esp_log.h"
+#include "esp_err.h"
+#include "freeRTOS/FreeRTOS.h"
+#include "mbedtls/base64.h"
+
+void app_main(void)
+{
+
+}
---
+++ path.sh
@@ -0,0 +1,1 @@
+.  /home/seahi/Desktop/ESP/ESP32/esp-idf/export.sh
